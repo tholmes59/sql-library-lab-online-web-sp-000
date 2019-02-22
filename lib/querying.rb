27858343f871789e63ec94b02c91ef39a103cede
Tom Holmes <tholmes59@gmail.com>
2019-02-22 04:00:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series INNER JOIN characters ON series.id = characters.series_id;"
+  "SELECT series.title FROM series INNER JOIN characters ON series.id = characters.series_id WHERE characters.species = 'human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
