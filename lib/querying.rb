7cc9da0b56eaf7d090048a069d572dbdb32bb3d7
Tom Holmes <tholmes59@gmail.com>
2019-02-22 02:20:02 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, characters.motto FROM characters HAVING MAX(LENGTH(characters.motto));"
+  "SELECT characters.name, MAX(LENGTH(characters.motto)) FROM characters;"
 end
 
 def select_value_and_count_of_most_prolific_species
