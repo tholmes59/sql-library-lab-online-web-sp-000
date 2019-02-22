36ab04b2216979ea6fd108c56de71105001f6d02
Tom Holmes <tholmes59@gmail.com>
2019-02-22 00:33:26 +0000

    Automatically backed up by Learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,20 +1,23 @@
 CREATE TABLE series (
   id INTEGER PRIMARY KEY,
-  
+  title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER
 );
 
 CREATE TABLE subgenres (
   id INTEGER PRIMARY KEY,
-  
+  name TEXT
 );
 
 CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
-  
+  name TEXT
 );
 
 CREATE TABLE books (
   id INTEGER PRIMARY KEY,
+  title TEXT,
   
 );
 
