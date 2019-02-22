71e4cc90fc80745d8ebf9b0e09782c568db75b8a
Tom Holmes <tholmes59@gmail.com>
2019-02-22 03:44:42 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -15,3 +15,6 @@ INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1,
 INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
 
 
+def select_name_and_series_subgenres_of_authors
+  "SELECT authors.name, subgenres.name FROM "
+end
