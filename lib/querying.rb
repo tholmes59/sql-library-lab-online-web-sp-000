3ef99d6f1b9f1f70c69f5f6a1f0e9ef0b4b76d28
Tom Holmes <tholmes59@gmail.com>
2019-02-22 03:33:26 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,7 +7,7 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species) FROM characters ORDER BY species LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
